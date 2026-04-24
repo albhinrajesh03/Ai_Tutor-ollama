cd091da13f8c867588e18ea87e98ee80313197f5
albhinrajesh03 <161609477+albhinrajesh03@users.noreply.github.com>
2026-04-24 06:43:25 +0530

    Update pdf_loader.py

--- pdf_loader.py
+++ pdf_loader.py
@@ -3,10 +3,8 @@ from pypdf import PdfReader
 def load_pdf(path):
     reader = PdfReader(path)
     text = ""
-
     for page in reader.pages:
         page_text = page.extract_text()
         if page_text:
             text += page_text + "\n"
-
     return text

49951740b9a74473a19069d07b06ade67d5b8d21
albhinrajesh03 <161609477+albhinrajesh03@users.noreply.github.com>
2026-05-06 13:08:06 +0530

    Update faiss_rag.py

--- faiss_rag.py
+++ faiss_rag.py
@@ -5,8 +5,17 @@ import faiss
 model=SentenceTransformer('all-MiniLM-L6-v2')
 
 def prepare_chunks(chunks):
-    encoded=model.encode(chunks)
+    encoded=model.encode(chunks).astype("float32")
     return encoded
 
 def retrieve(question,chunks,encodings):
-    question_encoding=model.encode(question)
+    question_encoding=model.encode(question).astype("float32")
+
+    dimension=encodings.shape[1]
+    index=faiss.IndexFlatL2(dimension)
+
+    index.add(encodings)
+
+    distance, indices=index.search(question_encoding,2)
+
+    return chunks[indices[0][0]]

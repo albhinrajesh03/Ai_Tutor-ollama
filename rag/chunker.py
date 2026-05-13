54e8e3e9a8d329f951c1c0b72f40891848f2ef6d
albhinrajesh03 <161609477+albhinrajesh03@users.noreply.github.com>
2026-05-13 15:27:58 +0530

    Delete rag directory

--- rag/chunker.py
+++
@@ -1,9 +0,0 @@
-def split_text(text, chunk_size=500, overlap=100):
-    chunks = []
-    i = 0
-
-    while i < len(text):
-        chunks.append(text[i:i+chunk_size])
-        i = i + chunk_size - overlap
-
-    return chunks
